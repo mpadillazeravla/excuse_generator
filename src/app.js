2cb31cb70b76b81d941716fc59844dae20338806
mpadillazeravla <mikepadilla82@gmail.com>
2022-05-10 09:00:21 +0000

    added innerHTML

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let excusa =
+    who[Math.floor(Math.random() * 4)] +
+    " " +
+    action[Math.floor(Math.random() * 4)] +
+    " " +
+    what[Math.floor(Math.random() * 3)] +
+    " " +
+    when[Math.floor(Math.random() * 5)];
+
+  return excusa;
 };
+
+excuse.innerHTML = onload();
